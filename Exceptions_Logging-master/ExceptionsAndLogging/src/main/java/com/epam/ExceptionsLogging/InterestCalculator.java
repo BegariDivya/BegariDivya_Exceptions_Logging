1a807bac13f9d52930db20077f98d7b1dd80e2c9
BegariDivya <52556409+BegariDivya@users.noreply.github.com>
2020-03-05 14:47:05 +0530

    Add files via upload

---
+++ Exceptions_Logging-master/ExceptionsAndLogging/src/main/java/com/epam/ExceptionsLogging/InterestCalculator.java
@@ -0,0 +1,9 @@
+package com.epam.ExceptionsLogging;
+
+public class InterestCalculator {
+	public static void main(String[] args) {
+		ConsructionLogger.CostEstimation();
+		SimpleInterest.calculateInterest();
+	}
+
+}
